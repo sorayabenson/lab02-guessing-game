6826cd6082587c0e86b580c00495b60284e9060f
Soraya Benson <soraya.benson@gmail.com>
2021-01-12 13:31:40 -0800

    html skeleton, basic css, dom elements linked

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+const numberInput = document.getElementById('numberInput');
+const remainingGuessesText = document.getElementById('remainingGuesses');
+const guessButton = document.getElementById('guessButton');
+const resultsMessageText = document.getElementById('resultsMessage');
 
 // initialize state
 
