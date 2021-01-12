c1d615c564f12ce69f1599a21de8834ed1f15c7e
Soraya Benson <soraya.benson@gmail.com>
2021-01-12 15:11:55 -0800

    button disabled after correct answer and after 4 guesses works

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ guessButton.addEventListener('click', () => {
     const tooHighMessage = `too high, try again!`;
     const tooLowMessage = `too low, try again!`;
     const correctAnswerMessage = `that's the number!`;
+    const lostMessage = `sorry the answer was ${randomNumberSelector}, please try again.`;
 
     if (userGuess < randomNumberSelector) {
         resultsMessageText.textContent = tooLowMessage;
@@ -24,20 +25,25 @@ guessButton.addEventListener('click', () => {
     }
     else if (userGuess == randomNumberSelector) {
         resultsMessageText.textContent = correctAnswerMessage;
+        remainingGuessesText.hidden = true;
+        guessButton.disabled = true;
     };
 
-    remainingGuessesText.textContent = guessesRemain--;
+    remainingGuessesText.textContent = `You have ${guessesRemain--} trys left.`;
 
-    console.log(randomNumberSelector);
+    if (guessesRemain === 0) {
+        guessButton.disabled = true;
+        resultsMessageText.textContent = lostMessage;
+
+        console.log(randomNumberSelector);
+    };
+
 })
 
 //     - addEventListener to the submit button
 
         // remaining guesses decrease by 1 (--)
 
-        // update view
-           // if guess is < n display 'too high' in results span
-            // if guess is > n display 'too low' in results span
             // if guess is correct party!
                 // disable input
         // disable input if run out of guesses + loser message.
