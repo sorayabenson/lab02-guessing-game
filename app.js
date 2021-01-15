fd18af9d22c9d17ab3c9fd7b57fd669d44b608f4
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-14 16:04:24 -0800

    fix the functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const numberInput = document.getElementById('numberInput');
 const remainingGuessesText = document.getElementById('remainingGuesses');
 const guessButton = document.getElementById('guessButton');
 const resultsMessageText = document.getElementById('resultsMessage');
+const resetButton = document.getElementById('resetButton');
 
 // initialize state
 let randomNumberSelector = Math.ceil(Math.random() * 20);
@@ -11,33 +12,42 @@ let guessesRemain = 4;
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () => {
 
-    const userGuess = numberInput.value;
+    const userGuess = Number(numberInput.value);
     const tooHighMessage = `too high, try again!`;
     const tooLowMessage = `too low, try again!`;
     const correctAnswerMessage = `that's the number!`;
     const lostMessage = `sorry the answer was ${randomNumberSelector}, please try again.`;
 
-    if (userGuess < randomNumberSelector) {
-        resultsMessageText.textContent = tooLowMessage;
-    }
-    else if (userGuess > randomNumberSelector) {
-        resultsMessageText.textContent = tooHighMessage;
-    }
-    else if (userGuess === randomNumberSelector) {
+
+    if (userGuess === randomNumberSelector) {
         resultsMessageText.textContent = correctAnswerMessage;
         remainingGuessesText.hidden = true;
         guessButton.disabled = true;
     }
+    else if (userGuess < randomNumberSelector) {
+        guessesRemain--;
+        resultsMessageText.textContent = tooLowMessage;
+    }
+    else if (userGuess > randomNumberSelector) {
+        guessesRemain--;
+        resultsMessageText.textContent = tooHighMessage;
 
-    --guessesRemain;
+    }
     remainingGuessesText.textContent = `Trys left: ${guessesRemain}`;
-
     if (guessesRemain === 0) {
         resultsMessageText.textContent = lostMessage;
         guessButton.disabled = true;
     }
 
 
-
-    console.log(randomNumberSelector);
+    resetButton.style.display = 'block';
 });
+
+resetButton.addEventListener('click', () => {
+    guessesRemain = 4;
+    remainingGuessesText.textContent = `Trys left: ${guessesRemain}`;
+    resultsMessageText.textContent = '';
+    randomNumberSelector = Math.ceil(Math.random() * 20);
+    numberInput.value = '';
+});
+console.log(randomNumberSelector);
