10f8b6534709502a0ed7185ce4fa4d7fbcea1e03
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-14 16:07:29 -0800

    clean up code

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ guessButton.addEventListener('click', () => {
     const correctAnswerMessage = `that's the number!`;
     const lostMessage = `sorry the answer was ${randomNumberSelector}, please try again.`;
 
-
     if (userGuess === randomNumberSelector) {
         resultsMessageText.textContent = correctAnswerMessage;
         remainingGuessesText.hidden = true;
@@ -31,23 +30,22 @@ guessButton.addEventListener('click', () => {
     else if (userGuess > randomNumberSelector) {
         guessesRemain--;
         resultsMessageText.textContent = tooHighMessage;
-
     }
     remainingGuessesText.textContent = `Trys left: ${guessesRemain}`;
+
     if (guessesRemain === 0) {
         resultsMessageText.textContent = lostMessage;
         guessButton.disabled = true;
     }
 
-
     resetButton.style.display = 'block';
 });
 
 resetButton.addEventListener('click', () => {
+
     guessesRemain = 4;
     remainingGuessesText.textContent = `Trys left: ${guessesRemain}`;
     resultsMessageText.textContent = '';
     randomNumberSelector = Math.ceil(Math.random() * 20);
     numberInput.value = '';
 });
-console.log(randomNumberSelector);
